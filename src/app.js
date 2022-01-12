aa8b01edb9c150e5849dba6c525e1c130de872c0
Lalith Medury <lalith.aakash970@gmail.com>
2022-01-13 04:31:11 +0530

    added cors headers

--- src/app.js
+++ src/app.js
@@ -11,12 +11,20 @@ const errorHandler = require('./middleware/errorHandler');
 
 const app = express();
 
+
+
 require('dotenv').config();
 app.use(helmet()); // https://expressjs.com/en/advanced/best-practice-security.html#use-helmet
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 app.use('/names', nameRouter);
 app.use('/', indexRouter);
